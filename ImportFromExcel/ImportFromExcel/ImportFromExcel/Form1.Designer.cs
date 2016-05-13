28fc866fbc69e71c6b5915d586d8abd8d7f444cf
Swornim Nepal <swornimnepal2000@gmail.com>
2016-05-13 16:59:30 +0545

    initial commit

---
+++ ImportFromExcel/ImportFromExcel/ImportFromExcel/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ImportFromExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.rbHeaderYes = new System.Windows.Forms.RadioButton();
+            this.rbHeaderNo = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(260, 245);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(43, 263);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(107, 49);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Import from Excel";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // rbHeaderYes
+            // 
+            this.rbHeaderYes.AutoSize = true;
+            this.rbHeaderYes.Location = new System.Drawing.Point(204, 265);
+            this.rbHeaderYes.Name = "rbHeaderYes";
+            this.rbHeaderYes.Size = new System.Drawing.Size(43, 17);
+            this.rbHeaderYes.TabIndex = 3;
+            this.rbHeaderYes.TabStop = true;
+            this.rbHeaderYes.Text = "Yes";
+            this.rbHeaderYes.UseVisualStyleBackColor = true;
+            // 
+            // rbHeaderNo
+            // 
+            this.rbHeaderNo.AutoSize = true;
+            this.rbHeaderNo.Location = new System.Drawing.Point(204, 288);
+            this.rbHeaderNo.Name = "rbHeaderNo";
+            this.rbHeaderNo.Size = new System.Drawing.Size(39, 17);
+            this.rbHeaderNo.TabIndex = 4;
+            this.rbHeaderNo.TabStop = true;
+            this.rbHeaderNo.Text = "No";
+            this.rbHeaderNo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Header";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 342);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbHeaderNo);
+            this.Controls.Add(this.rbHeaderYes);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.RadioButton rbHeaderYes;
+        private System.Windows.Forms.RadioButton rbHeaderNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ImportFromExcel/ImportFromExcel/ImportFromExcel/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Data.OleDb;
+
+
+namespace ImportFromExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+        DialogResult dr=   openFileDialog1.ShowDialog();
+            if (dr==DialogResult.OK)
+            {
+        {
+            string filePath = openFileDialog1.FileName;
+            string extension = Path.GetExtension(filePath);
+            string header = rbHeaderYes.Checked ? "YES" : "NO";
+            string conStr, sheetName;
+
+            conStr = string.Empty;
+            switch (extension)
+            {
+
+                case ".xls": //Excel 97-03
+                    conStr = string.Format(Excel03ConString, filePath, header);
+                    break;
+
+                case ".xlsx": //Excel 07
+                    conStr = string.Format(Excel07ConString, filePath, header);
+                    break;
+            }
+
+            //Get the name of the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                    con.Close();
+                }
+            }
+
+            //Read Data from the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        DataTable dt = new DataTable();
+                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+
+                        //Populate DataGridView.
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+        }
+                
+            }
+        }
+
+       
+    }
+}
